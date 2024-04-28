08d51a7eaf220ebcabc30cf28f5af43b7b8d8b92
johnmarkkk <johnmarkglomar@gmail.com>
2024-04-28 08:01:33 +0800

    assigned input value to empty

--- password.js
+++ password.js
@@ -15,22 +15,23 @@ let history = [];
 
 const button = document.querySelector("button");
 const input = document.querySelector("input");
-
 const paragraph = document.querySelector(".password");
-
 const content = document.querySelector(".history .content");
 const i = document.querySelector("i");
 const clear = document.querySelector(".history .clear");
 
+// to clear history
 clear.addEventListener('click', () => {
     const paragraphAll = content.querySelectorAll("p");
     for(let i = 0; i < paragraphAll.length; i++){
         paragraphAll[i].remove();
     }
     history = [];
+    input.value = "";
 });
-paragraph.addEventListener('click', () => {
 
+// for password generated history
+paragraph.addEventListener('click', () => {
     if(history.length !== 0){
         paragraph.innerHTML = "";
         content.style.display = 'block';
@@ -52,6 +53,7 @@ paragraph.addEventListener('click', () => {
 
 button.addEventListener('click', generate);
 
+// use to generate random passwords with 12 characters only
 function generate(){
     generatePassword = "";
     for(let i = 0; i<passwordLenght; i++){
